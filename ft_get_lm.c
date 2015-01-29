5d7090f8501cdea8c5f478c5e214dcd08f1ed6f6
Sophie POCHEZ <spochez@e2r3p8.42.fr>
2015-01-29 04:02:25 +0100

    Up norme

--- ft_get_lm.c
+++ ft_get_lm.c
@@ -6,7 +6,7 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/21 10:00:38 by spochez           #+#    #+#             */
-/*   Updated: 2015/01/28 22:16:45 by spochez          ###   ########.fr       */
+/*   Updated: 2015/01/29 03:59:14 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ int		ft_get_lm(char *fmt)
 {
 	int		lm;
 
-	printf("LMFMT = %s\n", fmt);
 	while (*fmt != '%')
 		(*fmt)--;
 	while (!ft_isdigit(*fmt) && *fmt)
