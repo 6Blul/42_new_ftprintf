d16bd3bfae096a159b2e09baa3105e905adfcc70
Sophie POCHEZ <spochez@e2r3p10.42.fr>
2015-01-31 07:19:38 +0100

    Up fill lm

--- ft_fill_tab.c
+++ ft_fill_tab.c
@@ -6,7 +6,7 @@
 /*   By: spochez <spochez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/20 10:56:19 by spochez           #+#    #+#             */
-/*   Updated: 2015/01/29 03:45:46 by spochez          ###   ########.fr       */
+/*   Updated: 2015/01/31 07:19:01 by spochez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int		*fill_iflags(int *tab, char *fmt, intmax_t arg)
 			tab[1] = 1;
 		if (*fmt == ' ' && arg > 0)
 			tab[2] = 1;
-		if (ft_isdigit(*fmt) && (*(fmt + 1)) != '.' && !ft_isdigit(*(fmt + 1)))
+		if (ft_isdigit(*fmt) && (*(fmt - 1)) != '.' && !ft_isdigit(*(fmt + 1)))
 			tab[3] = 1;
 		if (*fmt == '-')
 			tab[4] = 1;
@@ -60,7 +60,7 @@ int		*fill_uiflags(int *tab, char *fmt)
 			tab[0] = 1;
 		if (*fmt == '#')
 			tab[3] = 1;
-		if (ft_isdigit(*fmt) && (*(fmt + 1)) != '.' && !ft_isdigit(*(fmt + 1)))
+		if (ft_isdigit(*fmt) && (*(fmt - 1)) != '.' && !ft_isdigit(*(fmt + 1)))
 			tab[4] = 1;
 		if (*fmt == '-')
 			tab[5] = 1;
@@ -79,7 +79,7 @@ int		*fill_vflags(int *tab, char *fmt)
 	{
 		if (*fmt == '.')
 			tab[0] = 1;
-		if (ft_isdigit(*fmt) && (*(fmt + 1)) != '.' && !ft_isdigit(*(fmt + 1)))
+		if (ft_isdigit(*fmt) && (*(fmt - 1)) != '.' && !ft_isdigit(*(fmt + 1)))
 			tab[1] = 1;
 		if (*fmt == '-')
 			tab[2] = 1;
